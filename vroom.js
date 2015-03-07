f0d67aa43e82d5ff70f5caed21add9262d4b1f55
ramirs <ramirs2015@gmail.com>
2015-03-07 01:14:54 -0800

    implemented loading div

--- vroom.js
+++ vroom.js
@@ -1,11 +1,12 @@
 "use strict";
 
 window.onload = function(){
+	$("#loading").fadeOut(500);
+
 	$("#ghub").hover(
 		function(){
 			$("#ghub_title").css("opacity", "100")
 		}, function(){
 			$("#ghub_title").css("opacity", "0");
 		}); 
-
 }
