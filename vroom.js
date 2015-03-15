0fc9cb97b354ea6f92c29ded34e3ab6edea4376d
ramirs <ramirs2015@gmail.com>
2015-03-15 01:45:28 -0700

    separated sections, added a METRIC SH*T TON of css + JS

--- vroom.js
+++ vroom.js
@@ -8,5 +8,16 @@ window.onload = function(){
 			$("#ghub_title").css("opacity", "100")
 		}, function(){
 			$("#ghub_title").css("opacity", "0");
-		}); 
+		});
+
+	$("#landing_next").click(function(){
+		console.log("scroll nao!");
+		$('html,body').animate({scrollTop:$("#about_section").offset().top}, 1500);
+	});
+	$("#about_next").click(function(){
+		$('html,body').animate({scrollTop:$("#links_section").offset().top}, 1500);
+	});
+	$("#email_link").click(function(){
+		$( this ).text("vroomtech@gmail.com");
+	});
 }
